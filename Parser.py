0638308da19e0ebc81e76ffca5702f9b01ed0376
Kim,Maya <maya.kim@intel.com>
2018-12-11 10:49:43 +0900

    Version_2

---
+++ Parser.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#pylint:disble = missing-docstring
+#pylint:disable = no-name-in-module
+
+import sys
+from enum import Enum, IntEnum
+import array
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QFile
+from main import *
+from LoadPicture import *
+from PIL import Image
+import inspect
+
+
+
+class YUVFormat(IntEnum):
+    YUYV_LE = 1
+    UYVY_LE = 2
+    YVYU_LE = 3
+    VYUY_LE = 4
+
+    YUYV_BE = 5
+    UYVY_BE = 6
+    YVYU_BE = 7
+    VYUY_BE = 8
+
+class RGBFormat(IntEnum):
+    BGR3_LE = 11
+    RGB3_LE = 12
+    XR24_LE = 13
+    RGBP_LE = 14
+
+    BGR3_BE = 15
+    RGB3_BE = 16
+    XR24_BE = 17
+    RGBP_BE = 18
+
+
+
+class _Parser:
+    __DECODE_MAP = {
+        #color:[bit per pixel, desc]
+        'YUV422':[16, "YUV 4:2:2"],
+        'RGB3':[24, "green 8bits, red 8bits, blue 8bits"],
+        'XR24':[32, "green 8bits, red 8bits, blue 8bits, alpha 8bits"],
+        'RGBP':[16, "green 6bits, red 5bits, blue 5bits"]
+        }
+
+
+    def getbpp(cls, color):
+        return cls.__DECODE_MAP[color][0]
+
+
+    def getsize(self):
+        print('Total file size(bytes):', int(self._filesize_))
+        print('Current file size(bytes):', int(self._bufsize_))
+        print('Current file w*h:', int(self._bufsize_ / (self._bpp_ / 8)))
+
+
+    def __init__(self, _filepath, _format, _imgwidth, _imgheight):
+        self._filepath_ = _filepath
+        self._format_ = _format
+        self._imgwidth_ = _imgwidth
+        self._imgheight_ = _imgheight
+        self._data_ = {}
+        self._filesize_ = None
+        self._bufsize_ = None
+        self._bpp_ = None
+
+
+    def decode(self, choice):
+        """doc string"""
+        width = self._imgwidth_
+        height = self._imgheight_
+        form = self._format_
+        filepath = self._filepath_
+        self._data_.update(choice)
+
+        f_val = open(filepath, "rb")
+
+        file = QFile(filepath)
+        self._filesize_ = file.size()  #f_size = w*h*bpp/3
+
+
+        if YUVFormat.YUYV_LE <= form <= YUVFormat.VYUY_BE:
+            self._bpp_ = self.getbpp('YUV422')
+            self._bufsize_ = width * height * (self._bpp_ / 8)
+            image_out = self.YUV422(width, height, form, f_val)
+
+        elif (
+                form == RGBFormat.BGR3_LE or form == RGBFormat.BGR3_BE
+                or form == RGBFormat.RGB3_LE or form == RGBFormat.RGB3_BE
+        ):
+            self._bpp_ = self.getbpp('RGB3')
+            self._bufsize_ = width * height * (self._bpp_ / 8)
+            image_out = self.RGB3(width, height, form, f_val)
+
+        elif form == RGBFormat.XR24_LE or form == RGBFormat.XR24_BE:
+            self._bpp_ = self.getbpp('XR24')
+            self._bufsize_ = width * height * (self._bpp_ / 8)
+            image_out = self.XRGB(width, height, f_val)
+
+        elif form == RGBFormat.RGBP_LE or form == RGBFormat.RGBP_BE:
+            self._bpp_ = self.getbpp('RGBP')
+            self._bufsize_ = width * height * (self._bpp_ / 8)
+            image_out = self.RGBP(width, height, f_val)
+
+
+        data = image_out.tobytes('raw', "RGB")
+        qim = QImage(data, image_out.size[0], image_out.size[1], QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(qim)
+        return pixmap
+
+
+    def YUV422(self, width, height, form, f_uyvy):
+        self.getsize()
+        image_out = Image.new("RGB", (width, height), (0,0,0))
+        pix = image_out.load()
+
+        for i in range(0, height):
+            for j in range(0, int(width/2)):
+                if form == YUVFormat.YUYV_LE or form == YUVFormat.YUYV_BE:
+                    pix_y1 = ord(f_uyvy.read(1))
+                    pix_u = ord(f_uyvy.read(1))
+                    pix_y2 = ord(f_uyvy.read(1))
+                    pix_v = ord(f_uyvy.read(1))
+                elif form == YUVFormat.UYVY_LE or form == YUVFormat.UYVY_BE:
+                    pix_u = ord(f_uyvy.read(1))
+                    pix_y1 = ord(f_uyvy.read(1))
+                    pix_v = ord(f_uyvy.read(1))
+                    pix_y2 = ord(f_uyvy.read(1))
+                elif form == YUVFormat.YVYU_LE or form == YUVFormat.YVYU_BE:
+                    pix_y1 = ord(f_uyvy.read(1))
+                    pix_v = ord(f_uyvy.read(1))
+                    pix_y2 = ord(f_uyvy.read(1))
+                    pix_u = ord(f_uyvy.read(1))
+                elif form == YUVFormat.VYUY_LE or form == YUVFormat.VYUY_BE:
+                    pix_v = ord(f_uyvy.read(1))
+                    pix_y1 = ord(f_uyvy.read(1))
+                    pix_u = ord(f_uyvy.read(1))
+                    pix_y2 = ord(f_uyvy.read(1))
+
+                (
+                    pix_y1, pix_y2, pix_u, pix_v
+                ) = self.choice_yuvval(
+                    pix_y1, pix_y2, pix_u, pix_v
+                )
+
+
+                red = 1.164 * (pix_y1-16) + 2.018 * (pix_u - 128)
+                green = 1.164 * (pix_y1-16) - 0.813 * (pix_v - 128) - 0.391 * (pix_u - 128)
+                blue = 1.164 * (pix_y1-16) + 1.596 * (pix_v - 128)
+                pix[j*2, i] = int(blue), int(green), int(red)
+
+                red = 1.164 * (pix_y2-16) + 2.018 * (pix_u - 128)
+                green = 1.164 * (pix_y2-16) - 0.813 * (pix_v - 128) - 0.391 * (pix_u - 128)
+                blue = 1.164 * (pix_y2-16) + 1.596 * (pix_v - 128)
+                pix[j*2+1, i] = int(blue), int(green), int(red)
+
+
+        return image_out
+
+
+    def choice_yuvval(self, _y1, _y2, _u, _v):
+        if self._data_['y'] == 0:
+            _y1 = 16
+            _y2 = 16
+        if self._data_['u'] == 0:
+            _u = 128
+        if self._data_['v'] == 0:
+            _v = 128
+        return (_y1, _y2, _u, _v)
+
+
+
+    def RGB3(self, width, height, form, f_rgb):
+        self.getsize()
+        image_out = Image.new("RGB", (width, height), (0,0,0))
+        pix = image_out.load()
+
+        for i in range(0, height):
+            for j in range(0, int(width)):
+                if form == RGBFormat.BGR3_LE or form == RGBFormat.BGR3_BE:
+                    pix_r = ord(f_rgb.read(1))
+                    pix_g = ord(f_rgb.read(1))
+                    pix_b = ord(f_rgb.read(1))
+
+                elif form == RGBFormat.RGB3_LE or form == RGBFormat.RGB3_BE:
+                    pix_b = ord(f_rgb.read(1))
+                    pix_g = ord(f_rgb.read(1))
+                    pix_r = ord(f_rgb.read(1))
+
+
+                (pix_b, pix_g, pix_r) = self.choice_rgbval(pix_b, pix_g, pix_r)
+
+                red = pix_r
+                green = pix_g
+                blue = pix_b
+
+                pix[j, i] = int(blue), int(green), int(red)
+
+        return image_out
+
+
+
+    def XRGB(self, width, height, f_rgb):
+        self.getsize()
+        image_out = Image.new("RGB", (width, height), (0,0,0))
+        pix = image_out.load()
+
+        for i in range(0, height):
+            for j in range(0, int(width)):
+                pix_b = ord(f_rgb.read(1))
+                pix_g = ord(f_rgb.read(1))
+                pix_r = ord(f_rgb.read(1))
+                pix_a = ord(f_rgb.read(1))
+
+                (pix_b, pix_g, pix_r) = self.choice_rgbval(pix_b, pix_g, pix_r)
+
+                pix_a = 0
+                red = (pix_a * (pix_r / 255) + ((1 - pix_a) * (pix_r / 255))) * 255
+                green = (pix_a * (pix_g / 255) + ((1 - pix_a) * (pix_g / 255))) * 255
+                blue = (pix_a * (pix_b / 255) + ((1 - pix_a) * (pix_b / 255))) * 255
+
+                pix[j, i] = int(blue), int(green), int(red)
+
+        return image_out
+
+
+
+    def RGBP(self, width, height, f_rgb):
+        self.getsize()
+        image_out = Image.new("RGB", (width, height), (0,0,0))
+        pix = image_out.load()
+
+        for i in range(0, height):
+            for j in range(0, int(width)):
+                pix_h = ord(f_rgb.read(1))
+                pix_l = ord(f_rgb.read(1))
+
+                value = pix_l*256 + pix_h
+
+                red = (value & 0xF800) >> 8
+                green = (value & 0x7E0) >> 3
+                blue = (value & 0x1F) << 3
+
+                (blue, green, red) = self.choice_rgbval(blue, green, red)
+
+                pix[j, i] = int(blue), int(green), int(red)
+
+
+        return image_out
+
+
+
+    def choice_rgbval(self, pix_b, pix_g, pix_r):
+        if self._data_['b'] == 0:
+            pix_r = 0
+        if self._data_['g'] == 0:
+            pix_g = 0
+        if self._data_['r'] == 0:
+            pix_b = 0
+
+        return (pix_b, pix_g, pix_r)
---
+++ main.py
@@ -0,0 +1,430 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#pylint:disble=missing-docstring
+#pylint:disable=no-name-in-module
+"""
+
+This script converts raw image to other image format
+Form implementation generated from reading ui file 'widget.ui'
+python: 3.6
+
+"""
+import sys
+import os
+
+from enum import Enum, IntEnum
+from PyQt5 import (Qt, QtCore, QtGui, QtWidgets, uic)
+from PyQt5.uic import loadUi
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from PIL import Image
+
+
+from LoadPicture import *
+import Parser
+
+
+THINGS = os.path.dirname(os.path.abspath(__file__))
+L = lambda f: os.path.join(THINGS, f)
+
+
+class YUVFormat(IntEnum):
+    YUYV_LE = 1
+    UYVY_LE = 2
+    YVYU_LE = 3
+    VYUY_LE = 4
+
+    YUYV_BE = 5
+    UYVY_BE = 6
+    YVYU_BE = 7
+    VYUY_BE = 8
+
+
+class RGBFormat(IntEnum):
+    BGR3_LE = 11
+    RGB3_LE = 12
+    XR24_LE = 13
+    RGBP_LE = 14
+
+    BGR3_BE = 15
+    RGB3_BE = 16
+    XR24_BE = 17
+    RGBP_BE = 18
+
+
+class MainWindow(QMainWindow):
+    count = 0
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.pyqt_ui = uic.loadUi(L('MainGUI.ui'), self)
+        self.action_icon()
+        self.connect_action()
+
+        self.filepath = None
+        self.format = 1
+        self._format = 1
+        self.imgwidth = 400
+        self.imgheight = 400
+        self.factor = 1.0
+        self.pix = None
+
+
+    def action_icon(self):
+        icon_open = QIcon()
+        icon_save = QIcon()
+        icon_zoomin = QIcon()
+        icon_zoomout = QIcon()
+        icon_help = QIcon()
+        icon_about = QIcon()
+        icon_exit = QIcon()
+
+        icon_open.addPixmap(QPixmap('open.png'))
+        icon_save.addPixmap(QPixmap('save.png'))
+        icon_zoomin.addPixmap(QPixmap('zoomin.png'))
+        icon_zoomout.addPixmap(QPixmap('zoomout.png'))
+        icon_help.addPixmap(QPixmap('help.png'))
+        icon_about.addPixmap(QPixmap('about.png'))
+        icon_exit.addPixmap(QPixmap('exit.png'))
+
+        self.actionOpen.setIcon(icon_open)
+        self.actionSave_As.setIcon(icon_save)
+        self.actionZoom_in.setIcon(icon_zoomin)
+        self.actionZoom_out.setIcon(icon_zoomout)
+        self.actionHelp.setIcon(icon_help)
+        self.actionAbout.setIcon(icon_about)
+        self.actionExit.setIcon(icon_exit)
+
+
+    def connect_action(self):
+        self.actionOpen.triggered.connect(self.open_dialog)
+        self.actionSave_As.triggered.connect(self.save_dialog)
+        self.actionExit.triggered.connect(qApp.quit)
+        self.actionZoom_in.triggered.connect(self.zoom_in)
+        self.actionZoom_out.triggered.connect(self.zoom_out)
+
+        self.comb_format.currentIndexChanged.connect(self.set_format)
+
+        self.checkbox_y.clicked.connect(self.asign_format)
+        self.checkbox_u.clicked.connect(self.asign_format)
+        self.checkbox_v.clicked.connect(self.asign_format)
+        self.checkbox_r.clicked.connect(self.asign_format)
+        self.checkbox_g.clicked.connect(self.asign_format)
+        self.checkbox_b.clicked.connect(self.asign_format)
+        self.checkbox_swap.clicked.connect(self.swap_format)
+
+        self.radiobutton_be.clicked.connect(self.match_format)
+        self.radiobutton_le.clicked.connect(self.match_format)
+
+        self.LineEdit_width.textEdited.connect(self.update_size)
+        self.LineEdit_height.textEdited.connect(self.update_size2)
+        self.apply_button.clicked.connect(self.asign_format)
+
+        self.pushbutton_autodetection.clicked.connect(self.auto_detect)
+
+
+
+    def open_dialog(self):
+        self.fname, _ = QFileDialog.getOpenFileName(self, 'Open file')
+        self.filepath = self.fname
+        self.statusbar.showMessage("Successfully Loaded: {}".format(self.fname))
+
+        self.radiobutton_le.setChecked(True)
+
+
+        if self.format > 0 and self.format < 9:
+            self.group_yuv.setEnabled(True)
+            self.group_rgb.setEnabled(False)
+
+            self.checkbox_y.setChecked(True)
+            self.checkbox_u.setChecked(True)
+            self.checkbox_v.setChecked(True)
+
+            self.checkbox_r.setChecked(False)
+            self.checkbox_g.setChecked(False)
+            self.checkbox_b.setChecked(False)
+
+
+        elif self.format > 10 and self.format < 19:
+            self.group_yuv.setEnabled(False)
+            self.group_rgb.setEnabled(True)
+
+            self.checkbox_r.setChecked(True)
+            self.checkbox_g.setChecked(True)
+            self.checkbox_b.setChecked(True)
+
+            self.checkbox_y.setChecked(False)
+            self.checkbox_u.setChecked(False)
+            self.checkbox_v.setChecked(False)
+            self.checkbox_swap.setChecked(False)
+
+
+        self.asign_format()
+
+
+
+    def checkbox_state(self):
+        before_format = self._format
+        current_format = self.format
+
+        before_group = int(before_format/10)
+        current_group = int(current_format/10)
+
+        if self.format > 0 and self.format < 9:
+            self.group_yuv.setEnabled(True)
+            self.group_rgb.setEnabled(False)
+        elif self.format > 10 and self.format < 19:
+            self.group_yuv.setEnabled(False)
+            self.group_rgb.setEnabled(True)
+
+        if before_group != current_group:
+            if current_group == 0:
+                self.checkbox_y.setChecked(True)
+                self.checkbox_u.setChecked(True)
+                self.checkbox_v.setChecked(True)
+
+                self.checkbox_r.setChecked(False)
+                self.checkbox_g.setChecked(False)
+                self.checkbox_b.setChecked(False)
+                self.checkbox_swap.setChecked(False)
+            elif current_group == 1:
+                self.checkbox_y.setChecked(False)
+                self.checkbox_u.setChecked(False)
+                self.checkbox_v.setChecked(False)
+
+                self.checkbox_r.setChecked(True)
+                self.checkbox_g.setChecked(True)
+                self.checkbox_b.setChecked(True)
+                self.checkbox_swap.setChecked(False)
+
+
+
+    def save_dialog(self):
+        self.fname, _ = QFileDialog.getSaveFileName(self, 'Save file')
+        self.statusbar.showMessage("Successfully saved: {}".format(self.fname))
+
+
+    def set_format(self, i):
+        if i == 0:
+            self.format = YUVFormat.YUYV_LE
+        elif i == 1:
+            self.format = YUVFormat.UYVY_LE
+        elif i == 2:
+            self.format = YUVFormat.YVYU_LE
+        elif i == 3:
+            self.format = YUVFormat.VYUY_LE
+
+        elif i == 4:
+            self.format = RGBFormat.BGR3_LE
+        elif i == 5:
+            self.format = RGBFormat.RGB3_LE
+        elif i == 6:
+            self.format = RGBFormat.XR24_LE
+        elif i == 7:
+            self.format = RGBFormat.RGBP_LE
+
+        self.checkbox_state()
+        self.match_format()
+
+
+    def asign_format(self):
+        pa = Parser._Parser(self.filepath, self.format, self.imgwidth, self.imgheight)
+        self._format = self.format
+
+        if self.format > 0 and self.format < 9:
+            data = {'y':1, 'u':1, 'v':1}
+            if (
+                    self.format == YUVFormat.YUYV_LE or self.format == YUVFormat.UYVY_LE
+                    or self.format == YUVFormat.YVYU_BE or self.format == YUVFormat.VYUY_BE
+                ):
+                if not self.checkbox_y.isChecked():
+                    data['y'] = 0
+                if not self.checkbox_u.isChecked():
+                    data['u'] = 0
+                if not self.checkbox_v.isChecked():
+                    data['v'] = 0
+            elif (
+                    self.format == YUVFormat.YVYU_LE or self.format == YUVFormat.VYUY_LE
+                    or self.format == YUVFormat.YUYV_BE or self.format == YUVFormat.UYVY_BE
+                ):
+                if not self.checkbox_y.isChecked():
+                    data['y'] = 0
+                if not self.checkbox_u.isChecked():
+                    data['v'] = 0
+                if not self.checkbox_v.isChecked():
+                    data['u'] = 0
+        elif self.format > 10 and self.format < 19:
+            data = {'r':1, 'g':1, 'b':1}
+            if (
+                    self.format == RGBFormat.BGR3_LE or self.format == RGBFormat.RGB3_LE
+                    or self.format == RGBFormat.XR24_BE or self.format == RGBFormat.RGBP_BE
+                ):
+                if not self.checkbox_r.isChecked():
+                    data['r'] = 0
+                if not self.checkbox_g.isChecked():
+                    data['g'] = 0
+                if not self.checkbox_b.isChecked():
+                    data['b'] = 0
+            elif (
+                    self.format == RGBFormat.XR24_LE or self.format == RGBFormat.RGBP_LE
+                    or self.format == RGBFormat.BGR3_BE or self.format == RGBFormat.RGB3_BE
+                ):
+                if not self.checkbox_r.isChecked():
+                    data['r'] = 0
+                if not self.checkbox_g.isChecked():
+                    data['g'] = 0
+                if not self.checkbox_b.isChecked():
+                    data['b'] = 0
+
+        _pixmap = pa.decode(data)
+        self.pix = _pixmap
+        self.label_img.setPixmap(self.pix)
+        self.LineEdit_width.setText(str(self.imgwidth))
+        self.LineEdit_height.setText(str(self.imgheight))
+
+
+    def swap_format(self):
+        if self.radiobutton_le.isChecked():
+            if self.checkbox_swap.isChecked() or not self.checkbox_swap.isChecked():
+                if self.format == YUVFormat.YUYV_LE:
+                    self.format = YUVFormat.YVYU_LE
+                elif self.format == YUVFormat.UYVY_LE:
+                    self.format = YUVFormat.VYUY_LE
+                elif self.format == YUVFormat.YVYU_LE:
+                    self.format = YUVFormat.YUYV_LE
+                elif self.format == YUVFormat.VYUY_LE:
+                    self.format = YUVFormat.UYVY_LE
+        elif self.radiobutton_be.isChecked():
+            if self.checkbox_swap.isChecked() or not self.checkbox_swap.isChecked():
+                if self.format == YUVFormat.YUYV_BE:
+                    self.format = YUVFormat.YVYU_BE
+                elif self.format == YUVFormat.UYVY_BE:
+                    self.format = YUVFormat.VYUY_BE
+                elif self.format == YUVFormat.YVYU_BE:
+                    self.format = YUVFormat.YUYV_BE
+                elif self.format == YUVFormat.YVYU_BE:
+                    self.format = YUVFormat.YUYV_BE
+
+        self.asign_format()
+
+
+    def match_format(self):
+        if self.format > 0 and self.format < 9:
+            if self.radiobutton_le.isChecked():
+                if self.format == YUVFormat.VYUY_BE:
+                    self.format = YUVFormat.YUYV_LE
+                elif self.format == YUVFormat.YVYU_BE:
+                    self.format = YUVFormat.UYVY_LE
+                elif self.format == YUVFormat.UYVY_BE:
+                    self.format = YUVFormat.YVYU_LE
+                elif self.format == YUVFormat.YUYV_BE:
+                    self.format = YUVFormat.VYUY_LE
+            elif self.radiobutton_be.isChecked():
+                if self.format == YUVFormat.YUYV_LE:
+                    self.format = YUVFormat.VYUY_BE
+                elif self.format == YUVFormat.UYVY_LE:
+                    self.format = YUVFormat.YVYU_BE
+                elif self.format == YUVFormat.YVYU_LE:
+                    self.format = YUVFormat.UYVY_BE
+                elif self.format == YUVFormat.VYUY_LE:
+                    self.format = YUVFormat.YUYV_BE
+
+        elif self.format > 10 and self.format < 19:
+            if self.radiobutton_le.isChecked():
+                if self.format == RGBFormat.RGBP_BE:
+                    self.format = RGBFormat.BGR3_LE
+                elif self.format == RGBFormat.XR24_BE:
+                    self.format = RGBFormat.RGB3_LE
+                elif self.format == RGBFormat.RGB3_BE:
+                    self.format = RGBFormat.XR24_LE
+                elif self.format == RGBFormat.BGR3_BE:
+                    self.format = RGBFormat.RGBP_LE
+            elif self.radiobutton_be.isChecked():
+                if self.format == RGBFormat.BGR3_LE:
+                    self.format = RGBFormat.RGBP_BE
+                elif self.format == RGBFormat.RGB3_LE:
+                    self.format = RGBFormat.XR24_BE
+                elif self.format == RGBFormat.XR24_LE:
+                    self.format = RGBFormat.RGB3_BE
+                elif self.format == RGBFormat.RGBP_LE:
+                    self.format = RGBFormat.BGR3_BE
+
+        self.asign_format()
+
+
+    def load_to_sub(self, picture):
+        MainWindow.count = MainWindow.count+1
+
+        sub = QMdiSubWindow(self)
+        loaded_picture = LoadPicture(picture, sub)
+
+        sub.setWidget(loaded_picture)
+        sub.setObjectName("Load_Picture_window")
+        sub.setWindowTitle("New Photo"+str(MainWindow.count))
+        self.mdiArea.addSubWindow(sub)
+
+        sub.show()
+        sub.resize(500, 500)
+        loaded_picture.log.MousePixmapSignal.connect(self.update_pixel)
+
+
+    def update_pixel(self, point, color):
+        self.UserInput_PixelValue_X.setText("{}".format(point.x()))
+        self.UserInput_PixelValue_Y.setText("{}".format(point.y()))
+
+        self.UserInput_PixelValue_R.setText("{}".format(color.red()))
+        self.UserInput_PixelValue_G.setText("{}".format(color.green()))
+        self.UserInput_PixelValue_B.setText("{}".format(color.blue()))
+
+
+    def auto_detect(self):
+        for i in range(4):
+            self.format = i + 1
+            pa = Parser._Parser(self.filepath, self.format, self.imgwidth, self.imgheight)
+            _pixmap = pa.decode_yuv()
+            self.pix = _pixmap
+            self.load_to_sub(self.pix)
+
+
+    def update_size(self):
+        if self.LineEdit_width.text():
+            self.imgwidth = int(self.LineEdit_width.text())
+
+    def update_size2(self):
+        if self.LineEdit_height.text():
+            self.imgheight = int(self.LineEdit_height.text())
+
+
+    def zoom_in(self):
+        self.factor += 0.05
+        _width = self.imgwidth
+        _height = self.imgheight
+        _width = int(self.imgwidth * self.factor)
+        _height = int(self.imgheight * self.factor)
+        self.label_img.setPixmap(self.pix.scaled(_width, _height, Qt.KeepAspectRatio))
+        self.LineEdit_width.setText(str(_width))
+        self.LineEdit_height.setText(str(_height))
+
+
+    def zoom_out(self):
+        self.factor -= 0.05
+        _width = self.imgwidth
+        _height = self.imgheight
+        _width = int(self.imgwidth * self.factor)
+        _height = int(self.imgheight * self.factor)
+        self.label_img.setPixmap(self.pix.scaled(_width, _height, Qt.KeepAspectRatio))
+        self.LineEdit_width.setText(str(_width))
+        self.LineEdit_height.setText(str(_height))
+
+
+
+
+if __name__ == '__main__':
+    APP = QApplication(sys.argv)
+    WINDOW = MainWindow()
+    WINDOW.setWindowTitle('Raw Image viewer')
+    WINDOW.show()
+    sys.exit(APP.exec_())
+
